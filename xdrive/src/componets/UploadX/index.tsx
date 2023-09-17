5ae16fd7c3cff3d5a6b531fabc060211d03f0d0d
KULDEEP DHANGAR <90776144+kuldeep1A@users.noreply.github.com>
2023-09-17 23:01:18 +0530

    showing uploading progress...

--- xdrive/src/componets/UploadX/index.tsx
+++ xdrive/src/componets/UploadX/index.tsx
@@ -24,21 +24,22 @@ export default function UploadX() {
                     <div className="relative inline-block">
                         <input onChange={(event) => uploadFile(event)} type="file" className="file-input w-full max-w-xs ml-2.5 mr-2.5 mb-2 mt-2" />
                         {isVisibleL ? 
-                        <div className={styles.loading}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100">
-                                <circle cx="35" cy="50" r="5" fill="#ffffff">
-                                    <animate attributeName="opacity" from="1" to="0" dur="1s" begin="0s" repeatCount="indefinite" />
-                                </circle>
-                                <circle cx="50" cy="50" r="5" fill="#ffffff">
-                                    <animate attributeName="opacity" from="1" to="0" dur="1s" begin="0.2s" repeatCount="indefinite" />
-                                </circle>   
-                                <circle cx="65" cy="50" r="5" fill="#ffffff">
-                                    <animate attributeName="opacity" from="1" to="0" dur="1s" begin="0.4s" repeatCount="indefinite" />
-                                </circle>
-                            </svg>
-                            <div className="radial-progress" style={{"--value" as any: Progress}}>{Progress}%</div>
-
-                        </div>: <></>}
+                        <>
+                            <div className={styles.loading}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100">
+                                    <circle cx="35" cy="50" r="5" fill="#ffffff">
+                                        <animate attributeName="opacity" from="1" to="0" dur="1s" begin="0s" repeatCount="indefinite" />
+                                    </circle>
+                                    <circle cx="50" cy="50" r="5" fill="#ffffff">
+                                        <animate attributeName="opacity" from="1" to="0" dur="1s" begin="0.2s" repeatCount="indefinite" />
+                                    </circle>   
+                                    <circle cx="65" cy="50" r="5" fill="#ffffff">
+                                        <animate attributeName="opacity" from="1" to="0" dur="1s" begin="0.4s" repeatCount="indefinite" />
+                                    </circle>
+                                </svg>
+                            </div>
+                            <div className={styles.progressing}>{Progress}%</div>
+                        </>: <></>}
                     </div>
                 </>
             : <></>  }
