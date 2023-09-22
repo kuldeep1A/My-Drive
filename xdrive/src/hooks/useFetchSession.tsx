da2582314cb0a30c2ed464e36d5476b4cf3f66e1
KULDEEP DHANGAR <90776144+kuldeep1A@users.noreply.github.com>
2023-09-22 19:21:05 +0530

    deleted files and folder

--- xdrive/src/hooks/useFetchSession.tsx
+++
@@ -1,7 +0,0 @@
-import { useSession } from "next-auth/react"
-
-export const useFetchSession=() => {
-    const { data: session } = useSession();
-
-    return session;
-}
--- xdrive/src/pages/api/addFilex.tsx
+++
@@ -1,7 +0,0 @@
-import { type NextApiRequest, type NextApiResponse } from "next";
-
-const addFilex = (req: NextApiRequest, res:NextApiResponse) => {
-    res.status(200).json({text: "dflkj"});
-}
-
-export default addFilex;
