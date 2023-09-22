7c25f2ee0dc3ecb98d0915a56ec8a4587d744423
KULDEEP DHANGAR <90776144+kuldeep1A@users.noreply.github.com>
2023-09-22 17:17:19 +0530

    name changed

---
+++ kdrive/src/componets/HomeX/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import NavBar from "../NavBar";
+import UploadX from "../UploadX";
+import ShowFilesx from "../ShowFilesx";
+import { useFetchSession } from "@/hooks/useFetchSession";
+import SignIn from "../SignInX";
+
+export default function HomeX(){
+    const session = useFetchSession();
+    return (
+        <>
+            <NavBar/>
+            {session ? 
+                <>
+                    <UploadX parentId={""} />
+                    <ShowFilesx parentId={""} />
+                </> : <SignIn/> }
+        </>
+   )
+}
---
+++ kdrive/src/componets/SignInX/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Styles from './SignIn.module.scss';
+
+export default function SignIn() {  
+  return (
+    <>
+        <div className={Styles.CenterSigIn}>
+            <span className={Styles.KDrive}>
+              <div className={Styles.KDrive_child}>
+                <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem] px-10">
+                    <span className="text-[hsl(180,100%,70%)]">X</span> Drive
+                </h1>
+              </div>
+            </span>
+        </div>
+    </>
+  )
+}
