a44a79ffabc27721916cc9bc1486b214d8bdba04
KULDEEP DHANGAR <90776144+kuldeep1A@users.noreply.github.com>
2023-09-23 13:27:10 +0530

    is possibly 'undefined'

--- kdrive/src/componets/NavBar/index.tsx
+++ kdrive/src/componets/NavBar/index.tsx
@@ -25,14 +25,14 @@ export default function NavBar() {
                 <div>
                     <div className={Styles.authBtn}>
                         {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
-                        {session ? <> <Image src={`${session?.user.image}`} draggable={false} className="rounded-xl" width={50} height={50} alt="logo" /> <Button onClick={() => signOut()} btnClass={'btn-primary'} title='Sign Out' /> </> : <Button onClick={() => signIn()} btnClass={'btn-primary'} title='Sign In' />}
+                        {session ? <> <Image src={`${session?.user?.image}`} draggable={false} className="rounded-xl" width={50} height={50} alt="logo" /> <Button onClick={() => signOut()} btnClass={'btn-primary'} title='Sign Out' /> </> : <Button onClick={() => signIn()} btnClass={'btn-primary'} title='Sign In' />}
                     </div>
                 </div>
             </div>
             {session ?
                 <div className={`px-10 py-1 ${Styles.Info}`}>
-                    <h1>Name: {session?.user.name}</h1>
-                    <h1>Email: {session?.user.email}</h1>
+                    <h1>Name: {session?.user?.name}</h1>
+                    <h1>Email: {session?.user?.email}</h1>
                 </div> : <div></div>}
         </>
     )
