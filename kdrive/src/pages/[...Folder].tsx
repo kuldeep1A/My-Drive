66ca169556772298c6f943c57e0728cdf209f9f4
KULDEEP DHANGAR <90776144+kuldeep1A@users.noreply.github.com>
2023-09-23 15:15:26 +0530

    remove link

--- kdrive/src/pages/[...Folder].tsx
+++ kdrive/src/pages/[...Folder].tsx
@@ -15,10 +15,6 @@ export default function Folder() {
             <Head>
                 <title>K Drive</title>
                 <meta name="description" content="K-Drive storage" />
-                <link rel="icon" href="/favicons/favicon.ico" />
-                <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
             </Head>
             <main className="min-h-screen select-none bg-gradient-to-b from-[#2e026d] to-[#15162c]">
                 <NavBar />
