868127b2fa6f2bbc7e77ce51e9c61588bd11dafd
KULDEEP DHANGAR <90776144+kuldeep1A@users.noreply.github.com>
2023-09-25 12:38:59 +0530

    drive logo

--- kdrive/src/pages/index.tsx
+++ kdrive/src/pages/index.tsx
@@ -7,6 +7,11 @@ export default function Home() {
       <Head>
         <title>K Drive</title>
         <meta name="description" content="K-Drive storage" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <main className="min-h-screen select-none bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <HomeX />
