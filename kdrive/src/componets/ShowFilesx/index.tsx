694c4192029972fd6beb5db2c80c6e5b7cc07c31
KULDEEP DHANGAR <90776144+kuldeep1A@users.noreply.github.com>
2023-10-05 11:36:36 +0530

    send email on broswer

--- kdrive/src/componets/ShowFilesx/index.tsx
+++ kdrive/src/componets/ShowFilesx/index.tsx
@@ -155,9 +155,7 @@ export default function ShowFilesx({ parentId }: FolderStructure) {
     const subject = "Confirmation kdrive";
     const body =
       'This is your confirmation that you are authorized to use "kdrive," with approval granted by Administrator.';
-    const mailtoLink = `mailto:${encodeURIComponent(
-      AdministratorEmail,
-    )}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+    const mailtoLink = `https://mail.google.com/mail/?view=cm&fs=1&to=${encodeURIComponent(recipientEmail)}&subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
 
     window.location.href = mailtoLink;
   };
